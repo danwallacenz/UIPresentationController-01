9eaa9963115e04fe31e2d2ecb2bac6891cdf647c
Daniel Wallace <daniel.wallace.nz@gmail.com>
2015-02-16 12:57:51 +1300

    initial commit

---
+++ UIPresentationController-01/ReadOnlyViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ReadOnlyViewController.swift
+//  UIPresentationController-01
+//
+//  Created by Daniel Wallace on 16/02/15.
+//  Copyright (c) 2015 nz.co.danielw. All rights reserved.
+//
+
+import UIKit
+
+class ReadOnlyViewController: UIViewController {
+
+    @IBAction func EditButtonPressed() {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
